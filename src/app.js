7c26b4ec969db3d821f3671ac490dcd4d8aff3d4
PJ Kindt <pjkindt@PJs-MacBook-Pro.local>
2020-09-20 17:05:57 +0200

    remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -12,14 +12,6 @@ import 'react-dates/lib/css/_datepicker.css'
 
 const store = configureStore()
 
-store.dispatch(addExpense({ description: 'water bill', amount: 4500 }))
-store.dispatch(addExpense({ description: 'gas bill', createdAt: 1000 }))
-store.dispatch(addExpense({ description: 'water rent', amount: 109500 }))
-
-const state = store.getState()
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-console.log(visibleExpenses)
-
 // console.log(store.getState())
 // with the next lines each component of AppRouter will be able to interact with the store
 const jsx = (
